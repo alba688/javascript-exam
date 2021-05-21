e530f5f54f8a2190e6fb2e46ebb3838ba45775ef
Alexandra Baker <baker.alexandra88@gmail.com>
2021-05-21 10:52:57 +0200

    search function to meny page

--- html/Modules/MenyModule.js
+++ html/Modules/MenyModule.js
@@ -5,64 +5,65 @@ const MenyModule = (function() {
             Bilde: "pizza-margherita.jpg",
             Navn: "Pizza Marinara",
             Pris: 180,
-            Type: "pizza",
+            Type: "Pizza",
             Beskrivelse: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi in officia nemo ullam. Perferendis sit assumenda, porro illo nostrum adipisci?"
         },
         {
             Bilde: "pizza-ost.jpg",
             Navn: "Pizza Ost",
             Pris: 150,
-            Type: "pizza",
+            Type: "Pizza",
             Beskrivelse: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi in officia nemo ullam. Perferendis sit assumenda, porro illo nostrum adipisci?"
         },
         {
             Bilde: "pizza-paprika.jpg",
             Navn: "Pizza Paprika",
             Pris: 200,
-            Type: "pizza",
+            Type: "Pizza",
             Beskrivelse: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi in officia nemo ullam. Perferendis sit assumenda, porro illo nostrum adipisci?"
         },
         {
             Bilde: "pizza-deluxe.jpg",
             Navn: "Pizza Deluxe",
             Pris: 230,
-            Type: "pizza",
+            Type: "Pizza",
             Beskrivelse: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi in officia nemo ullam. Perferendis sit assumenda, porro illo nostrum adipisci?"
         },
         {
             Bilde: "is-sjokolade.jpg",
             Navn: "Sjokolade Is",
             Pris: 60,
-            Type: "dessert",
+            Type: "Dessert",
             Beskrivelse: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi in officia nemo ullam. Perferendis sit assumenda, porro illo nostrum adipisci?"
         },
         {
             Bilde: "is-vanilje.jpg",
             Navn: "Vanilje Is",
             Pris: 50,
-            Type: "dessert",
+            Type: "Dessert",
             Beskrivelse: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi in officia nemo ullam. Perferendis sit assumenda, porro illo nostrum adipisci?"
         },
         {
             Bilde: "drikk-pepsi.jpg",
             Navn: "Pepsi Max",
             Pris: 30,
-            Type: "drikk",
+            Type: "Drikk",
             Beskrivelse: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi in officia nemo ullam. Perferendis sit assumenda, porro illo nostrum adipisci?"
         },
         {
             Bilde: "drikk-solo.jpg",
             Navn: "Solo",
             Pris: 30,
-            Type: "drikk",
+            Type: "Drikk",
             Beskrivelse: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi in officia nemo ullam. Perferendis sit assumenda, porro illo nostrum adipisci?"
         }
     ];
 
     const getAllMeny = () => MenyArray;
 
-    const getByMenyType = (input) => {
-        return MenyArray.filter(mObj => mObj.Type === input);
+    const getByMenyType = (inputType) => {
+        MenyArray.filter((menyObj) => menyObj.Type.toLowerCase() === inputType.toLowerCase());
+        //console.log(menyObj.Type);
     }
 
     return{getAllMeny, getByMenyType};
--- html/Scripts/meny.js
+++ html/Scripts/meny.js
@@ -1,24 +1,57 @@
 import MenyModule from '../Modules/MenyModule.js'
 
 const menySection = document.getElementById("meny-section");
+const submitBtn = document.querySelector("#submit-btn"); submit-btn
+const cancelBtn = document.querySelector("#cancel-btn");
+const input = document.querySelector("#input-txt");
 
-let htmlTxt = "";
 
-MenyModule.getAllMeny().forEach(meny => {
-    htmlTxt += `
-        <article class="column is-3">
-            <div class="card">
-                <section class="card-image">
-                    <img src="../Images/Menu/${meny.Bilde}">
-                </section>
-                <section class="card-content">
-                    <h1 class="title">${meny.Navn}</h3>
-                    <h2 class="subtitle is-4">${meny.Pris} kr</p>
-                    <p class="is-size-6">${meny.Beskrivelse}</p>
-                </section>
-            </div>
-        </article>
-    `;
+function printMeny(){
+    let htmlTxt = "";
+    MenyModule.getAllMeny().forEach(meny => {
+        htmlTxt += `
+            <article class="column is-3">
+                <div class="card">
+                    <section class="card-image">
+                        <img src="../Images/Menu/${meny.Bilde}">
+                    </section>
+                    <section class="card-content">
+                        <h1 class="title">${meny.Navn}</h3>
+                        <h2 class="subtitle is-4">${meny.Pris} kr</p>
+                        <p class="is-size-6">${meny.Beskrivelse}</p>
+                    </section>
+                </div>
+            </article>
+        `;
+    })
+    menySection.innerHTML = htmlTxt;
+}
+
+
+printMeny();
+
+
+submitBtn.addEventListener("click", () => {
+    let htmlSearch = "";
+
+    MenyModule.getByMenyType(input.value).forEach((menyItem) => {
+        htmlSearch += `
+            <article class="column is-3">
+                <div class="card">
+                    <section class="card-image">
+                        <img src="../Images/Menu/${menyItem.Bilde}">
+                    </section>
+                    <section class="card-content">
+                        <h1 class="title">${menyItem.Navn}</h3>
+                        <h2 class="subtitle is-4">${menyItem.Pris} kr</p>
+                        <p class="is-size-6">${menyItem.Beskrivelse}</p>
+                    </section>
+                </div>
+            </article>
+        `;
+    })
+
+    menySection.innerHTML = htmlSearch;
 })
 
-menySection.innerHTML = htmlTxt;
+cancelBtn.addEventListener("click", printMeny());
