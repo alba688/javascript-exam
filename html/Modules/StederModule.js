0ec663fab6e3c8ea513f6590457b4c4e38f63831
Swintero <77145157+Swintero@users.noreply.github.com>
2021-05-23 20:47:59 +0200

    Steder

---
+++ html/Modules/StederModule.js
@@ -0,0 +1,25 @@
+const stederArray = [
+        {
+            Bilde: "../Images/Background/steder2.jpg",
+            LokasjonLink: "https://Google.com",
+            Lokasjon: "Placeholder Sted",
+            Tider: "11:00 - 23:00",
+            
+        },
+        {
+            Bilde: "../Images/Background/steder3.jpg",
+            LokasjonLink: "https://Google.com",
+            Lokasjon: "Placeholder Sted",
+            Tider: "11:00 - 23:00",
+            
+        },
+        {
+            Bilde: "../Images/Background/steder4.jpg",
+            LokasjonLink: "https://Google.com",
+            Lokasjon: "Placeholder Sted",
+            Tider: "11:00 - 23:00",
+            
+        }
+    ]
+
+export default stederArray;
