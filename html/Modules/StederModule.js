a1e84bc957ca5c92e507e1de72f49ee86221f074
Swintero <77145157+Swintero@users.noreply.github.com>
2021-05-24 15:30:34 +0200

    update, added locations

--- html/Modules/StederModule.js
+++ html/Modules/StederModule.js
@@ -1,23 +1,31 @@
 const stederArray = [
         {
             Bilde: "../Images/Background/steder2.jpg",
-            LokasjonLink: "https://Google.com",
+            LokasjonLink: "https://goo.gl/maps/FtT8q7SeGzKe2hkG8",
+            LokasjonBilde: "../Images/Icons/location.png",
             Lokasjon: "Bergen",
             Tider: "11:00 - 23:00",
+            Telefon: "12345678",
+            
             
         },
         {
             Bilde: "../Images/Background/steder3.jpg",
-            LokasjonLink: "https://Google.com",
+            LokasjonLink: "https://goo.gl/maps/FtT8q7SeGzKe2hkG8",
+            LokasjonBilde: "../Images/Icons/location.png",
             Lokasjon: "Trondheim",
             Tider: "11:00 - 23:00",
+            Telefon: "12345678"
+            
             
         },
         {
             Bilde: "../Images/Background/steder4.jpg",
-            LokasjonLink: "https://Google.com",
+            LokasjonLink: "https://goo.gl/maps/FtT8q7SeGzKe2hkG8",
+            LokasjonBilde: "../Images/Icons/location.png",
             Lokasjon: "Oslo",
             Tider: "11:00 - 23:00",
+            Telefon: "12345678"
             
         }
     ]
