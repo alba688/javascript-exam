9664f5054d3b94b0eb95dc7480918f377027783f
Alexandra Baker <baker.alexandra88@gmail.com>
2021-05-29 12:17:13 +0200

    hamburger menu and embedded maps

--- html/Modules/StederModule.js
+++ html/Modules/StederModule.js
@@ -1,37 +1,37 @@
 const stederArray = [
         {
             Bilde: "../Images/Background/steder2.jpg",
-            LokasjonLink: "https://goo.gl/maps/y27gjMsW6utvaWe9A",
+            LokasjonLink: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1971.3408957770448!2d5.320253151293274!3d60.390047232477265!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x463cfea9e1b60f2b%3A0x6c6dd1e298a6d483!2sOlav%20Kyrres%20gate%2045%2C%205014%20Bergen!5e0!3m2!1sen!2sno!4v1621707947060!5m2!1sen!2sno",
             LokasjonBilde: "../Images/Icons/location.png",
             Alt: "Lokasjon Bergen bilde",
-            LokasjonAdresse: "Kirkegata 24, 0107 Oslo",
+            LokasjonAdresse: "Olav Kyrres gate 45",
             Lokasjon: "Bergen",
             Tider: "11:00 - 23:00",
-            Telefon: "12345678",
+            Telefon: "123-45-678",
             
             
         },
         {
             Bilde: "../Images/Background/steder3.jpg",
-            LokasjonLink: "https://goo.gl/maps/FtT8q7SeGzKe2hkG8",
+            LokasjonLink: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1784.5913728207513!2d10.383105851435484!3d63.43028658262727!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x466d31850f20dc2d%3A0xaad426ec9619ecb9!2sKongens%20gate%2075%2C%207012%20Trondheim!5e0!3m2!1sen!2sno!4v1621707823265!5m2!1sen!2sno",
             LokasjonBilde: "../Images/Icons/location.png",
             Alt: "Lokasjon Trondheim bilde",
-            LokasjonAdresse: "Kalfarveien 78C, 5022 Bergen",
+            LokasjonAdresse: "Kongens gate 75",
             Lokasjon: "Trondheim",
             Tider: "11:00 - 23:00",
-            Telefon: "12345678"
+            Telefon: "123-45-678"
             
             
         },
         {
             Bilde: "../Images/Background/steder4.jpg",
-            LokasjonLink: "https://goo.gl/maps/hugzC1NwujgvRcDh9",
+            LokasjonLink: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1000.0288138334729!2d10.735005776042023!3d59.91459094001085!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46416e7d96221ed5%3A0x3e4a5ae684fb920!2sKarl%20Johans%20gate%2078%2C%200162%20Oslo!5e0!3m2!1sen!2sno!4v1621708020639!5m2!1sen!2sno",
             LokasjonBilde: "../Images/Icons/location.png",
             Alt: "Lokasjon Oslo bilde",
-            LokasjonAdresse: "Kongens gate 22, 0153 Oslo",
+            LokasjonAdresse: "Karl Johans gate 78",
             Lokasjon: "Oslo",
             Tider: "11:00 - 23:00",
-            Telefon: "12345678"
+            Telefon: "123-45-678"
             
         }
     ]
--- html/Scripts/Steder.js
+++ html/Scripts/Steder.js
@@ -4,8 +4,35 @@ const stederSection = document.getElementById("steder-section")
 
 
     let htmlTxt = "";
+
     stederArray.forEach(steder => {
         htmlTxt += `
+        <section class="columns mb-4">
+            <article class="column card is-8 m-4">
+                <div class="columns">
+                    <div class="column card-image is-one-half">
+                        <img src="${steder.Bilde}" alt="${steder.Alt}">
+                    </div>
+                    <div class="column card-content is-one-half">
+                        <h1 class="title">Gylden Pizza ${steder.Lokasjon}</h3>
+                        <h2 class="subtitle is-4">Address: ${steder.LokasjonAdresse}</p>
+                            <p class="is-size-6">Telefon: ${steder.Telefon}</p>
+                            <p class="is-size-6">Åpningstider: ${steder.Tider}</p>
+                        <br>
+                    </div>
+                </div>
+            </article>
+            <article class="column is-4">
+                <iframe src="${steder.LokasjonLink}" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+            </article>
+        </section>
+    `;
+    });
+    stederSection.innerHTML = htmlTxt;
+
+
+    /*
+    `
         <article class="column is-3">
             <div class="card">
                 <h2 class="title has-text-centered mt-4">${steder.Lokasjon}</h2>
@@ -32,9 +59,7 @@ const stederSection = document.getElementById("steder-section")
                 </section
             </div>
         </article>
-    `;
-    });
-    stederSection.innerHTML = htmlTxt;
-
+    `
+    */
 
 
